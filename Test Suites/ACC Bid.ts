2935d8c6fed791c75088a71c1ef5224c453132fe
ASUS <ASUS@LAPTOP-KVQOJMPC>
2019-11-11 14:30:47 +0700

    update task kritik&saran & laporkan bug

---
+++ Test Suites/ACC Bid.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ACC Bid</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d1546b9a-6cda-4fd7-9d7a-17b7ee85c5da</testSuiteGuid>
+   <testCaseLink>
+      <guid>2c69c445-f645-485f-a5cf-e3ac1ad3eefc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Task 18 (Kritik dan Saran)</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>3a143dbd-248f-4c91-9af0-0552a5c24895</id>
+         <iterationEntity>
+            <iterationType>SPECIFIC</iterationType>
+            <value>3</value>
+         </iterationEntity>
+         <testDataId>Data Files/task 18</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>3a143dbd-248f-4c91-9af0-0552a5c24895</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>input_text</value>
+         <variableId>b838c4dd-1a12-4da2-9713-687db3840815</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>3a143dbd-248f-4c91-9af0-0552a5c24895</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>expected</value>
+         <variableId>c943a604-2153-4d0b-83c1-c4f1a081922e</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7e9de925-3b7b-45fa-9765-05b56b04a84a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Task 19 (Laporkan Bug)</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>afc2193b-89c5-4e18-a150-0467debfbeb1</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/task 19</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>afc2193b-89c5-4e18-a150-0467debfbeb1</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>laporkan_bug</value>
+         <variableId>b1fb103c-883a-45c9-9556-7737ee41566f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>afc2193b-89c5-4e18-a150-0467debfbeb1</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>expected</value>
+         <variableId>f4e9a301-f52d-4abd-b2e5-8c43d9eef73e</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
