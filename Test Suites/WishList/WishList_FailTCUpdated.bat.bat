841f93042ee82c01cdfbc7df600f5df2569c46b6
Marvin6262 <marvinaj12@gmail.com>
2019-12-03 16:27:40 +0700

    Revert "Revert "Merge branch 'accbid' into tantan""
    This reverts commit 5c94f04701bb7ceb1ffec685825f5499e3c14046.

--- Test Suites/WishList/WishList_FailTCUpdated.bat.bat
+++ Test Suites/WishList/WishList_FailTCUpdated.bat.bat
@@ -1,1 +1,1 @@
-katalon -noSplash  -runMode=console -projectPath="C:\Users\Marvin\Documents\GitHub\ACCBid-Mobile\ACCBid.prj" -retry=0 -testSuitePath="Test Suites/WishList/WishList_20191111_165434" -executionProfile="default" -browserType="Android"
+katalon -noSplash  -runMode=console -projectPath="C:\Users\Marvin\Documents\GitHub\ACCBid-Mobile\ACCBid.prj" -retry=0 -testSuitePath="Test Suites/WishList/WishList_20191203_143653" -executionProfile="default" -browserType="Android"
